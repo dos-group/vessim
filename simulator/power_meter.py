6ad14daaf4f24e0f892c0f0558022dc2c0d03dc7
marvin.steinke <marvin.steinke@campus.tu-berlin.de>
2023-03-14 15:10:34 +0100

    implemented PhysicalPowerMeter class

--- simulator/power_meter.py
+++ simulator/power_meter.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 from typing import Tuple, Dict, Callable, Optional
+import paho.mqtt.client as mqtt
 
 
 PowerModel = Callable[[float], float]
@@ -31,9 +32,42 @@ class PowerMeter(ABC):
 
 
 class PhysicalPowerMeter(PowerMeter):
-    def node_power(self):
-        # TODO measure device power usage
-        return 10
+    def __init__(self, host: str = "localhost", port: int = 1883, keepalive: int = 60, name: Optional[str] = None):
+        """
+        Initializes a new instance of the PhysicalPowerMeter class, an MQTT
+        wrapper that serves as an adapter for physical nodes (HIL) to submit
+        their power usage.
+
+        :param host: The hostname or IP address of the MQTT broker. Default is "localhost".
+        :param port: The port number to use for the MQTT connection. Default is 1883.
+        :param keepalive: The maximum period in seconds allowed between communications with the MQTT broker. Default is 60.
+        """
+
+        super().__init__(name)
+        # create MQTT client instance
+        self.client = mqtt.Client()
+        # assign the on_connect and on_message methods to the MQTT client's corresponding attributes
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        # connect the MQTT client to the broker with the provided host, port, and keepalive values
+        self.client.connect(host, port=port, keepalive=keepalive)
+        # the last received power value from client
+        self.last_node_power = 0
+
+    # on_connect method that gets called when the MQTT client connects to the broker
+    def on_connect(self, client, userdata, flags, rc):
+        print(f"PhysicalPowerMeter: Connected with result code {str(rc)}")
+        # subscribe to the "node_power" topic
+        self.client.subscribe("node_power")
+
+    # on_message method that gets called when a message is received on the subscribed topic
+    def on_message(self, client, userdata, msg)
+        # decode the message payload and store it as float in last_node_power
+        self.last_node_power = float(msg.payload.decode())
+
+    # node_power method to retrieve the last received power value
+    def node_power(self) -> float:
+        return self.last_node_power
 
 
 class VirtualPowerMeter(PowerMeter, ABC):
