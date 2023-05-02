16583247e351845a8d92caaaab9bd0ce5dc2a394
marvin.steinke <marvin.steinke@campus.tu-berlin.de>
2023-05-02 10:56:49 +0200

    removed some code for better comprehension

--- carbon-aware_control_unit/carbon_aware_control_unit.py
+++ carbon-aware_control_unit/carbon_aware_control_unit.py
@@ -1,15 +1,15 @@
-from http_client import HTTPClient, HTTPClientError
+from http_client import HTTPClient
 import simpy
-from typing import Optional
+from typing import Dict
 
 
 class Battery:
-    """Initializes a Battery instance with the given initial state.
+    """Initializes a Battery instance that holds some info of the remote battery.
 
     Args:
-        soc: The initial state of the battery's state of charge.
-        min_soc: The minimum state of charge threshold for the battery.
-        grid_charge: The initial state of the battery's grid charge level.
+        soc: The initial state of the battery's state of charge in %.
+        min_soc: The minimum state of charge threshold for the battery in %.
+        grid_charge: The power which the battery is charged with from the public grid in W.
     """
 
     def __init__(self, soc: float = 0.0, min_soc: float = 0.0, grid_charge: float = 0.0) -> None:
@@ -27,9 +27,6 @@ class CarbonAwareControlUnit:
             manages, with node IDs as keys and node objects as values.
 
     Attributes:
-        ci: The current carbon intensity in gCO2eq/kWh.
-        solar: The current solar energy available in kWh.
-        battery: The battery object that stores excess energy.
         power_modes: The list of available power modes for the nodes.
         nodes_power_mode: A dictionary mapping node IDs to their current power modes.
         nodes: A dictionary representing the nodes that the Control
@@ -39,11 +36,7 @@ class CarbonAwareControlUnit:
     """
 
     def __init__(self, server_address: str, nodes: dict) -> None:
-        self.ci = 0.0
-        self.solar = 0.0
-        self.battery = Battery()
         self.power_modes = ['power-saving', 'normal', 'high performance']
-        self.nodes_power_mode = {}
         self.nodes = nodes
 
         self.client = HTTPClient(server_address)
@@ -68,66 +61,49 @@ class CarbonAwareControlUnit:
         Yields:
             A SimPy timeout event that delays the process by one unit of time.
         """
-        self.update_values()
+        battery = Battery(soc=self.client.get('/battery-soc'))
+        solar = self.client.get('/solar')
+        ci = self.client.get('/ci')
+        nodes_power_mode = {}
 
         # Set the minimum SOC of the battery based on the current time
         if self.env.now < 60*36:
-            self.set_battery(min_soc=0.3)
+            battery.min_soc = 0.3
         else:
-            self.set_battery(min_soc=0.6)
+            battery.min_soc = 0.6
 
         # Adjust the power modes of the nodes based on the current carbon intensity and battery SOC
-        if self.ci <= 200 or self.battery.soc > 0.8:
-            self.set_node_power_mode(self.nodes['aws'], 'high performance')
-            self.set_node_power_mode(self.nodes['raspi'], 'high performance')
-        elif self.ci >= 250 and self.battery.soc < self.battery.min_soc:
-            self.set_node_power_mode(self.nodes['aws'], 'power-saving')
-            self.set_node_power_mode(self.nodes['raspi'], 'power-saving')
+        if ci <= 200 or battery.soc > 0.8:
+            nodes_power_mode[self.nodes['aws']] = 'high performance'
+            nodes_power_mode[self.nodes['raspi']] = 'high performance'
+        elif ci >= 250 and battery.soc < battery.min_soc:
+            nodes_power_mode[self.nodes['aws']] = 'power-saving'
+            nodes_power_mode[self.nodes['raspi']] = 'power-saving'
         else:
-            self.set_node_power_mode(self.nodes['aws'], 'normal')
-            self.set_node_power_mode(self.nodes['raspi'], 'normal')
+            nodes_power_mode[self.nodes['aws']] = 'normal'
+            nodes_power_mode[self.nodes['raspi']] = 'normal'
 
         # Delay the process by one unit of time
+        self.send_battery(battery)
+        self.send_nodes_power_mode(nodes_power_mode)
         yield self.env.timeout(1)
 
 
-    def update_values(self) -> None:
-        """Updates all relevant attributes of the instance by by sending a GET request to the server.
-        """
-        self.battery.soc = self.client.get('/battery-soc')
-        self.solar = self.client.get('/solar')
-        self.ci = self.client.get('/ci')
-
-
-    def set_battery(self, min_soc: Optional[float] = None , grid_charge: Optional[float] = None) -> None:
-        """Sets the minimum SOC threshold and grid charge level of the battery by sending a put request to the server.
+    def send_battery(self, battery: Battery) -> None:
+        """Sends battery data to the VES API.
 
         Args:
-            min_soc: The new minimum SOC threshold to set.
-            grid_charge: The new grid charge level to set.
+            battery: An object containing the battery data to be sent.
         """
-        if not min_soc:
-            min_soc = self.battery.min_soc
-        if not grid_charge:
-            grid_charge = self.battery.grid_charge
-        try:
-            self.client.put('/ves/battery', {'min_soc': min_soc, 'grid_charge': grid_charge})
-            self.battery.min_soc = min_soc
-            self.battery.grid_charge = grid_charge
-        except HTTPClientError as e:
-            print(e)
+        self.client.put('/ves/battery', {'min_soc': battery.min_soc, 'grid_charge': battery.grid_charge})
 
 
-    def set_node_power_mode(self, node_id: int, power_mode: str) -> None:
-        """Sets the power mode of a specified node by sending a PUT request to the server.
+    def send_nodes_power_mode(self, nodes_power_mode: Dict[int, str]) -> None:
+        """Sends power mode data for nodes to the VES API.
 
         Args:
-            node_id: The ID of the node to set the power mode for.
-            power_mode: The new power mode to set (must be one of
-                'power-saving', 'normal', or 'high performance').
+            nodes_power_mode: A dictionary containing node IDs as keys and their respective power modes as values.
+
         """
-        assert power_mode in self.power_modes
-        try:
+        for node_id, power_mode in nodes_power_mode.items():
             self.client.put(f'/ves/nodes/{node_id}', {'power_mode': power_mode})
-        except HTTPClientError as e:
-            print(e)
