9e80522994b126a11dd3200eddb51914af8a5cf5
kilianp14 <Paul-Kilian@gmx.de>
2023-05-15 12:14:48 +0200

    Corrected test

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import main
 
 test_args = {
     "START": "2014-01-01 11:00:00",
-    "END": 62,  # 30 * 24 * 3600  # 10 days
+    "END": 61,  # 30 * 24 * 3600  # 10 days
     "GRID_FILE": "data/custom.json",  # "data/custom.json"  # 'data/demo_lv_grid.json'
     "SOLAR_DATA": "data/pv_10kw.csv",
     "CARBON_DATA": "data/ger_ci_testing.csv",
@@ -17,5 +17,5 @@ def test_solar_and_carbon_data_passing():
     main.main(test_args)
     with open("data.csv") as data_file:
         lines = data_file.readlines()
-        assert lines[60] == "0,0.60,126000.00,352.52,0.00,1356.00 "
-        assert lines[61] == "1,0.60,126000.00,352.90,0.00,1339.67 "
+        assert lines[60] == "59,0.60,126000.00,352.52,0.00,1356.00"
+        assert lines[61] == "60,0.60,126000.00,352.90,0.00,1339.67"
