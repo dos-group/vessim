cfa0fa508d6e65e2b91b842ff3e6592529598fb8
Philipp Wiesner <wiesner@tu-berlin.de>
2023-05-23 17:41:53 +0200

    some improvements to CarbonIntensityApi

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import pytest
 
-from vessim.components import CarbonIntensityAPI
+from vessim.components import CarbonIntensityApi
 
 
-class TestCarbonIntensityAPI:
+class TestCarbonIntensityApi:
 
     @pytest.fixture
-    def ci_api(self) -> CarbonIntensityAPI:
+    def ci_api(self) -> CarbonIntensityApi:
         index = [pd.to_datetime("2023-01-01T00:00:00"),
                  pd.to_datetime("2023-01-01T00:30:00"),
                  pd.to_datetime("2023-01-01T01:00:00")]
         data = pd.DataFrame({"a": [1, 2, 3], "b": [0, 3, 0]}, index=index)
-        return CarbonIntensityAPI(data)
+        return CarbonIntensityApi(data)
 
     def test_zones(self, ci_api):
         assert ci_api.zones() == ["a", "b"]
 
-    @pytest.mark.parametrize("now, zone, expected", [
+    @pytest.mark.parametrize("dt, zone, expected", [
         (pd.to_datetime("2023-01-01T00:00:00"), "a", 1),
         (pd.to_datetime("2023-01-01T00:00:10"), "a", 1),
         (pd.to_datetime("2023-01-01T01:00:00"), "a", 3),
@@ -25,8 +25,8 @@ class TestCarbonIntensityAPI:
         (pd.to_datetime("2023-01-01T00:29:59"), "b", 0),
         (pd.to_datetime("2023-01-01T00:30:00"), "b", 3),
     ])
-    def test_carbon_intensity_at(self, ci_api, now, zone, expected):
-        assert ci_api.carbon_intensity_at(now, zone) == expected
+    def test_carbon_intensity_at(self, ci_api, dt, zone, expected):
+        assert ci_api.carbon_intensity_at(dt, zone) == expected
 
     def test_carbon_intensity_at_fails_if_now_too_early(self, ci_api):
         with pytest.raises(ValueError):
--- vessim/components.py
+++ vessim/components.py
@@ -5,33 +5,40 @@ import pandas as pd
 Time = Union[int, float, str, datetime]
 
 
-class CarbonIntensityAPI:
-    def __init__(self, data: pd.DataFrame):
+class CarbonIntensityApi:
+    def __init__(self, data: pd.DataFrame, unit: str = "g_per_kWh"):
         """Service for querying the carbon intensity at different times and locations.
 
         Args:
             data: DataFrame with carbon intensity values. Each index represents a
                 timestamp and each column a location.
+            unit: Unit of the carbon intensity data: gCO2/kWh (`g_per_kWh`) or lb/MWh
+                (`lb_per_MWh`). Note that Vessim internally assumes gCO2/kWh, so choosing
+                lb/MWh will simply convert this data to gCO2/kWh.
         """
         self.data = data
+        if unit == "lb_per_MWh":
+            self.data = self.data * 0.45359237
+        elif unit != "g_per_kWh":
+            raise ValueError(f"Carbin intensity unit '{unit}' is not supported.")
 
     def zones(self) -> List:
         """Returns a list of all available zones."""
         return list(self.data.columns)
 
-    def carbon_intensity_at(self, now: Time, zone: str):
+    def carbon_intensity_at(self, dt: Time, zone: str) -> float:
         """Returns the carbon intensity at a given time and zone.
 
         If the queried timestamp is not available in the `data` dataframe, the last valid
         datapoint is being returned.
         """
         try:
-            location_carbon_intensity = self.data[zone]
+            zone_carbon_intensity = self.data[zone]
         except KeyError:
             raise ValueError(f"Cannot retrieve carbon intensity at zone '{zone}'.")
         try:
-            return location_carbon_intensity.loc[self.data.index.asof(now)]
+            return zone_carbon_intensity.loc[self.data.index.asof(dt)]
         except KeyError:
-            raise ValueError(f"Cannot retrieve carbon intensity at {now} in zone "
+            raise ValueError(f"Cannot retrieve carbon intensity at {dt} in zone "
                              f"'{zone}'.")
 
