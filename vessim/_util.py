b2982a2d28a02def5f3166ea068dbd719db4783c
Philipp Wiesner <wiesner@tu-berlin.de>
2023-06-12 00:13:24 +0200

    implemented Generator and TraceSimulator base class

--- vessim/_util.py
+++ vessim/_util.py
@@ -4,6 +4,9 @@ from typing import Union
 import pandas as pd
 
 
+Time = Union[int, float, str, datetime]
+
+
 class Clock:
     def __init__(self, sim_start: Union[str, datetime]):
         self.sim_start = pd.to_datetime(sim_start)
@@ -13,3 +16,14 @@ class Clock:
 
     def to_simtime(self, dt: datetime) -> int:
         return (dt - self.sim_start).seconds
+
+
+class TraceSimulator:
+
+    def __init__(self, data: Union[pd.Series, pd.DataFrame]):
+        self.data = data
+
+    def next_update(self, dt: Time):
+        current_index = self.data.index.asof(dt)
+        next_iloc = self.data.index.get_loc(current_index) + 1
+        return self.data.index[next_iloc]
