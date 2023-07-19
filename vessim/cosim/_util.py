83353bd37bb25e9e6eeecd190375aa1c82e3271f
marvin.steinke <marvin.steinke@campus.tu-berlin.de.de>
2023-07-19 12:37:07 +0200

    split is_attribute from is_below_threshold

--- vessim/cosim/_util.py
+++ vessim/cosim/_util.py
@@ -144,13 +144,14 @@ def disable_mosaik_warnings(behind_threshold: float):
         #print(record)
         is_warning = record["level"].name == "WARNING"
         is_mosaik_log = record["name"].startswith("mosaik")
+        is_attribute = record["function"] == "_check_attributes_values"
         is_below_threshold = (
-            record["function"] == "_check_attributes_values" or
-            record["function"] =="rt_check" and
+            record["function"] == "rt_check" and
             float(record["message"].split(' - ')[1].split('s')[0]) < behind_threshold
         )
-        return not (is_warning and is_mosaik_log and is_below_threshold)
+        return not (is_warning and is_mosaik_log and (is_below_threshold or is_attribute))
 
     # Add the filter to the logger
     logger.remove()
     logger.add(sys.stdout, filter=filter_record)
+
