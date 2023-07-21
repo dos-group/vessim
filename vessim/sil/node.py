2643fa72602ea4ebcf866bbb32512945b0723fc8
marvin.steinke <marvin.steinke@campus.tu-berlin.de.de>
2023-07-21 14:06:56 +0200

    adjusted docstring

--- vessim/sil/node.py
+++ vessim/sil/node.py
@@ -3,14 +3,10 @@ from typing import Set
 class Node:
     """Represents a physical or virtual computing node.
 
-    This class keeps track of nodes and assigns unique IDs to each new instance. It also
-    allows the setting of a power meter and power mode.
+    This class keeps track of nodes and assigns unique IDs to each new
+    instance. It also allows the setting of a power meter and power mode.
 
     Args:
-        address: The network address of the node.
-        power_mode: The power mode of the node. Default is "high performance".
-
-    Attributes:
         id: A unique ID assigned to each node.
         address: The network address of the node.
         port: The application port of the node api.
