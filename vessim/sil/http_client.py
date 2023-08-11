87dc537ebc8a0fad65d7a3bd5db2c383587c0565
marvin.steinke <marvin.steinke@campus.tu-berlin.de.de>
2023-08-11 11:06:17 +0200

    facilitated exception propagation from LoopThread to main thread; removed HTTPError exception, renaming

--- vessim/sil/http_client.py
+++ vessim/sil/http_client.py
@@ -3,15 +3,8 @@ import json
 from typing import Any, Dict
 
 
-class HTTPClientError(Exception):
-    def __init__(self, status_code: int, message: str = ""):
-        self.status_code = status_code
-        self.message = message
-        super().__init__(f"HTTP client error (status code: {status_code}): {message}")
-
-
-class HTTPClient:
-    """Class for making HTTP requests to the VESSIM API server.
+class HttpClient:
+    """Class for making HTTP requests to the Vessim API server.
 
     Args:
         server_address: The address of the server to connect to.
@@ -28,40 +21,33 @@ class HTTPClient:
             route: The path of the endpoint to send the request to.
 
         Raises:
-            HTTPClientError if the data could not be retrieved from route
+            HTTPError if response code is != 200.
 
         Returns:
             A dictionary containing the response.
         """
         response = requests.get(self.server_address + route)
-        if response.status_code == 200:
-            data = response.json() # assuming the response data is in JSON format
-            return data
-
-        raise HTTPClientError(
-            response.status_code,
-            f'Failed to retrieve data from {route}'
-        )
+        if response.status_code != 200:
+            response.raise_for_status()
+        data = response.json() # assuming the response data is in JSON format
+        return data
 
     def put(self, route: str, data: Dict[str, Any]) -> None:
         """Sends a PUT request to the server to update data.
 
         Raises:
-            HTTPClientError if the data could not be updated at route
+            HTTPError if response code is != 200.
 
         Args:
             route: The path of the endpoint to send the request to.
             data: The data to be updated, in dictionary format.
         """
-        headers = {'Content-type': 'application/json'}
+        headers = {"Content-type": "application/json"}
         response = requests.put(
             self.server_address + route,
             data=json.dumps(data),
             headers=headers
         )
         if response.status_code != 200:
-            raise HTTPClientError(
-                response.status_code,
-                f'Failed to update data at {route}'
-            )
+            response.raise_for_status()
 
--- vessim/sil/sil_interface.py
+++ vessim/sil/sil_interface.py
@@ -4,9 +4,9 @@ from typing import List, Optional
 from vessim.core.storage import Storage, DefaultStoragePolicy, StoragePolicy
 from vessim.cosim._util import VessimSimulator, VessimModel, simplify_inputs
 from vessim.sil.api_server import ApiServer, VessimApi
-from vessim.sil.http_client import HTTPClient, HTTPClientError
+from vessim.sil.http_client import HttpClient
 from vessim.sil.node import Node
-from vessim.sil.stoppable_thread import StoppableThread
+from vessim.sil.loop_thread import LoopThread
 
 
 class SilInterfaceSim(VessimSimulator):
@@ -93,14 +93,14 @@ class _SilInterfaceModel(VessimModel):
         self.api_server.wait_for_startup_complete()
 
         # init server values and wait for put request confirmation
-        self.http_client = HTTPClient(f"http://{api_host}:{api_port}")
+        self.http_client = HttpClient(f"http://{api_host}:{api_port}")
         self.http_client.put("/sim/update", {
             "solar": self.p_gen,
             "ci": self.ci,
             "battery_soc": self.storage.soc(),
         })
 
-        self.collector_thread = StoppableThread(self._api_collector, collection_interval)
+        self.collector_thread = LoopThread(self._api_collector, collection_interval)
         self.collector_thread.start()
 
     def _api_collector(self):
@@ -142,6 +142,7 @@ class _SilInterfaceModel(VessimModel):
                     self.updated_nodes.append(node)
 
     def step(self, time: int, inputs: dict) -> None:
+        self.collector_thread.propagate_exception()
         inputs = simplify_inputs(inputs)
         self.p_cons = inputs["p_cons"]
         self.p_gen = inputs["p_gen"]
@@ -150,27 +151,21 @@ class _SilInterfaceModel(VessimModel):
 
         # update values to the api server
         def update_api_server():
-            try:
-                self.http_client.put("/sim/update", {
-                    "solar": self.p_gen,
-                    "ci": self.ci,
-                    "battery_soc": self.storage.soc(),
-                })
-            except HTTPClientError as e:
-                print(e)
+            self.http_client.put("/sim/update", {
+                "solar": self.p_gen,
+                "ci": self.ci,
+                "battery_soc": self.storage.soc(),
+            })
         # use thread to not slow down simulation
         api_server_update_thread = Thread(target=update_api_server)
         api_server_update_thread.start()
 
         # update power mode for the node remotely
         for node in self.updated_nodes:
-            http_client = HTTPClient(f"{node.address}:{node.port}")
+            http_client = HttpClient(f"{node.address}:{node.port}")
 
             def update_node_power_model():
-                try:
-                    http_client.put("/power_mode", {"power_mode": node.power_mode})
-                except HTTPClientError as e:
-                    print(e)
+                http_client.put("/power_mode", {"power_mode": node.power_mode})
             # use thread to not slow down simulation
             node_update_thread = Thread(target=update_node_power_model)
             node_update_thread.start()
