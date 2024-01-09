b7e477613ec2b54c2bd2f0ebd9b94908e4a81cca
Amanda Malkowski <amanda061@windowslive.com>
2024-01-09 18:06:55 +0100

    reformatted code and changed pipeline for tests

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,13 +1,21 @@
 from _data import load_solar_data, load_carbon_data
 from vessim.core import TimeSeriesApi
-from vessim.cosim import Microgrid, Environment, ComputingSystem, Generator, Monitor, MockPowerMeter, SimpleBattery
+from vessim.cosim import (
+    Microgrid,
+    Environment,
+    ComputingSystem,
+    Generator,
+    Monitor,
+    MockPowerMeter,
+    SimpleBattery,
+)
 
 SIM_START = "2020-06-11 00:00:00"
 DURATION = 3600 * 24 * 2  # two days
 STORAGE = SimpleBattery(
     capacity=32 * 5 * 3600,  # 10Ah * 5V * 3600 := Ws
-    charge_level=32 * 5 * 3600 * .6,
-    min_soc=.6
+    charge_level=32 * 5 * 3600 * 0.6,
+    min_soc=0.6,
 )
 
 
@@ -23,13 +31,13 @@ def main(result_csv: str):
                 step_size=60,
                 power_meters=[
                     MockPowerMeter(name="mpm0", p=2.194),
-                    MockPowerMeter(name="mpm1", p=7.6)
-                ]
+                    MockPowerMeter(name="mpm1", p=7.6),
+                ],
             ),
             Generator(
                 name="solar",
                 step_size=60,
-                time_series_api=TimeSeriesApi(load_solar_data(sqm=0.4 * 0.5))
+                time_series_api=TimeSeriesApi(load_solar_data(sqm=0.4 * 0.5)),
             ),
         ],
         controllers=[monitor],
