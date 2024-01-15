c312dc8a5e5993fe0239439070e66eb4cec4ff9e
Philipp Wiesner <wiesner@tu-berlin.de>
2024-01-14 22:32:13 -0300

    minor data.py cleanup

--- vessim/data.py
+++ vessim/data.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from vessim._util import DatetimeLike, PandasObject
 
-DATASETS = {
+VESSIM_DATASETS = {
     "solcast2022_germany": {
         "actual": "solcast2022_germany_actual.csv",
         "forecast": "solcast2022_germany_forecast.csv",
@@ -28,50 +28,45 @@ DATASETS = {
 
 
 def _load_dataset(
-    dataset: Union[str, Dict],
-    data_dir: Path,
+    dataset: str,
     scale: float = 1.0,
     start_time: Optional[DatetimeLike] = None,
     use_forecast: bool = False,
 ) -> Dict:
     """Downloads a dataset from the vessim repository, unpacks it and loads data."""
-    if isinstance(dataset, str):
-        dataset_dict: Dict = DATASETS[dataset]
-    else:
-        dataset_dict = dataset
+    if dataset not in VESSIM_DATASETS:
+        raise ValueError(f"Dataset '{dataset}' not found. Available datasets are: "
+                         f"{', '.join(list(VESSIM_DATASETS.keys()))}")
 
-    required_files = [dataset_dict["actual"]]
+    dataset_config = VESSIM_DATASETS[dataset]
+    required_files = [dataset_config["actual"]]
     if use_forecast:
-        required_files.append(dataset_dict["forecast"])
+        required_files.append(dataset_config["forecast"])
 
-    dir_path = Path(data_dir or "").expanduser().resolve()
+    dir_path = Path().expanduser().resolve()
 
     if not _check_files(required_files, dir_path):
-        if "url" not in dataset_dict.keys():
-            raise RuntimeError("Data files could not be found locally.")
-
-        print("Required data files not present. Try downloading...")
+        print("Required data files not present locally. Try downloading...")
         os.makedirs(dir_path, exist_ok=True)
-
+        zip_path = dir_path / "dataset.zip"
         try:
-            urllib.request.urlretrieve(dataset_dict["url"], dir_path / "dataset.zip")
+            urllib.request.urlretrieve(dataset_config["url"], zip_path)
         except Exception:
-            raise RuntimeError(
-                f"Dataset could not be retrieved from url: {dataset_dict['url']}"
-            )
+            raise RuntimeError(f"Dataset could not be retrieved from url: "
+                               f"{dataset_config['url']}")
 
-        with ZipFile(dir_path / "dataset.zip", "r") as zip_ref:
+        with ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(path=dir_path)
-        os.remove(dir_path / "dataset.zip")
+        os.remove(zip_path)
         print("Successfully downloaded and unpacked data files.")
 
     actual = _read_data_from_csv(
-        dir_path / dataset_dict["actual"], index_cols=[0], scale=scale
+        dir_path / dataset_config["actual"], index_cols=[0], scale=scale
     )
 
     forecast: Optional[PandasObject] = None
     if use_forecast:
-        if dataset_dict.get("static_forecast", False):
+        if dataset_config.get("static_forecast", False):
             # There is only one timestamp present in the forecast (static forecast)
             index_cols: List[int] = [0]
         else:
@@ -79,7 +74,7 @@ def _load_dataset(
             index_cols = [0, 1]
 
         forecast = _read_data_from_csv(
-            dir_path / dataset_dict["forecast"], index_cols=index_cols, scale=scale
+            dir_path / dataset_config["forecast"], index_cols=index_cols, scale=scale
         )
 
     if start_time is not None:
@@ -89,15 +84,11 @@ def _load_dataset(
         if use_forecast:
             forecast = _shift_dataframe(forecast, shift)  # type: ignore
 
-    result = dict(
+    return dict(
         actual=actual,
-        fill_method=dataset_dict.get("fill_method", "ffill")
+        forecast=None if not use_forecast else forecast,
+        fill_method=dataset_config.get("fill_method", "ffill"),
     )
-
-    if use_forecast:
-        result["forecast"] = forecast
-
-    return result
 
 
 def _check_files(files: List[str], base_dir: Path) -> bool:
