d84e9f63d64fbdf8b7d7185b4890792175ab2144
Amanda Malkowski <amanda061@windowslive.com>
2024-01-22 14:22:29 +0100

    remove poetry.lock

--- vessim/cosim/controller.py
+++ vessim/cosim/controller.py
@@ -51,7 +51,7 @@ class Monitor(Controller):
         self,
         step_size: Optional[int] = None,
         monitor_storage=True,
-        monitor_grid_signals=True,
+        monitor_grid_signals=True
     ):
         super().__init__(step_size=step_size)
         self.monitor_storage = monitor_storage
