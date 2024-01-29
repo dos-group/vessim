e50604e95726dabb6cd9cf5c131037d98a658b14
marvin.steinke <marvin.steinke@campus.tu-berlin.de>
2024-01-29 12:55:11 +0100

    simplified controller example

--- examples/controller_example.py
+++ examples/controller_example.py
@@ -9,41 +9,28 @@ from vessim.cosim import (
     Controller,
     Microgrid,
     Environment,
-    DefaultStoragePolicy,
     MockPowerMeter,
     SimpleBattery,
 )
-
-POLICY = DefaultStoragePolicy()
-POWER_MODES = {  # according to paper
-    "mpm0": {"high performance": 2.964, "normal": 2.194, "power-saving": 1.781},
-    "mpm1": {"high performance": 8.8, "normal": 7.6, "power-saving": 6.8},
-}
 
 
 def main(result_csv: str):
     environment = Environment(sim_start=SIM_START)
     environment.add_grid_signal("carbon_intensity", HistoricalSignal(load_carbon_data()))
 
-    power_meters = [
-        MockPowerMeter(name="mpm0", p=2.194),
-        MockPowerMeter(name="mpm1", p=7.6),
+    power_meters: list = [
+        MockPowerMeter(name="mpm0", p=3),
+        MockPowerMeter(name="mpm1", p=7),
     ]
-    battery = SimpleBattery(capacity=100)
     monitor = Monitor()  # stores simulation result on each step
-    carbon_aware_controller = CarbonAwareController(
-        power_meters=power_meters,
-        battery=battery,
-        policy=POLICY,
-    )
+    power_meter_controller = PowerMeterController(power_meters=power_meters)
     microgrid = Microgrid(
         actors=[
             ComputingSystem(power_meters=power_meters),
             Generator(signal=HistoricalSignal(load_solar_data(sqm=0.4 * 0.5))),
         ],
-        storage=battery,
-        storage_policy=POLICY,
-        controllers=[monitor, carbon_aware_controller],
+        storage=SimpleBattery(capacity=1000, charge_level=500),
+        controllers=[monitor, power_meter_controller],
         zone="DE",
         step_size=60,  # global step size (can be overridden by actors or controllers)
     )
@@ -53,74 +40,19 @@ def main(result_csv: str):
     monitor.to_csv(result_csv)
 
 
-class CarbonAwareController(Controller):
-    def __init__(self, power_meters, battery, policy, step_size=None):
-        super().__init__(step_size)
+class PowerMeterController(Controller):
+    def __init__(self, power_meters: list[MockPowerMeter]):
+        super().__init__()
         self.power_meters = power_meters
-        self.battery = battery
-        self.policy = policy
 
     def step(self, time: int, p_delta: float, actors: dict):
-        """Performs a time step in the model."""
-        new_state = cacu_scenario(
-            time=time,
-            battery_soc=self.battery.soc(),
-            ci=self.grid_signals["carbon_intensity"].at(
-                self.clock.to_datetime(time), self.microgrid.zone
-            ),
-            node_names=[node.name for node in self.power_meters],
-        )
-        self.policy.grid_power = new_state["grid_power"]
-        self.battery.min_soc = new_state["battery_min_soc"]
-        assert {"mpm0", "mpm1"}.issubset({mpm.name for mpm in self.power_meters})
-        for node in self.power_meters:
-            node.set_power(
-                POWER_MODES[node.name][new_state["nodes_power_mode"][node.name]]
-            )
-
-
-def cacu_scenario(
-    time: int, battery_soc: float, ci: float, node_names: list[str]
-) -> dict:
-    """Calculate the power mode settings for nodes based on a scenario.
-
-    This function simulates the decision logic of a Carbon-Aware Control Unit
-    (CACU) by considering battery state-of-charge (SOC), time, and carbon
-    intensity (CI).
-
-    Args:
-        time: Time in minutes since some reference point or start.
-        battery_soc: Current state of charge of the battery.
-        ci: Current carbon intensity.
-        node_names: A list of node IDs for which the power mode needs to be
-            determined.
-
-    Returns:
-        A dictionary containing:
-            - battery_min_soc: Updated minimum state of charge value based on
-              the given time.
-            - grid_power: Power to be drawn from the grid.
-            - nodes_power_mode: A dictionary with node IDs as keys and their
-              respective power modes ('high performance', 'normal', or
-              'power-saving') as values.
-    """
-    new_state = {}
-    time_of_day = time % (3600 * 24)
-    if 11 * 3600 <= time_of_day < 24 * 3600 and battery_soc >= 0.6:
-        new_state["battery_min_soc"] = 0.6
-    else:
-        new_state["battery_min_soc"] = 0.3
-
-    new_state["grid_power"] = 20 if ci <= 200 and battery_soc < 0.6 else 0
-    new_state["nodes_power_mode"] = {}
-    for node_name in node_names:
-        if ci <= 200 or battery_soc > 0.8:
-            new_state["nodes_power_mode"][node_name] = "high performance"
-        elif ci >= 250 and battery_soc < 0.6:
-            new_state["nodes_power_mode"][node_name] = "power-saving"
-        else:
-            new_state["nodes_power_mode"][node_name] = "normal"
-    return new_state
+        for power_meter in self.power_meters:
+            pm = power_meter.measure()
+            if p_delta < 0:
+                if pm > 1:
+                    power_meter.set_power(pm - 1)
+            elif p_delta > 0:
+                power_meter.set_power(pm + 1)
 
 
 if __name__ == "__main__":
