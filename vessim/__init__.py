b35cecf80674f4593c833b2e9a41bf5002d7a69c
marvin.steinke <marvin.steinke@tu-berlin.de>
2024-05-22 16:43:05 +0200

    made all vessim imports top lvl

--- vessim/__init__.py
+++ vessim/__init__.py
@@ -1,22 +1,32 @@
 """A simulator for carbon-aware applications and systems."""
-from vessim import actor
-from vessim import controller
-from vessim import cosim
-from vessim import power_meter
-from vessim import signal
-from vessim import storage
+from vessim.actor import Actor, ComputingSystem, Generator
+from vessim.controller import Controller, Monitor
+from vessim.cosim import Microgrid, Environment
+from vessim.policy import MicrogridPolicy, DefaultMicrogridPolicy
+from vessim.power_meter import PowerMeter, MockPowerMeter
+from vessim.signal import Signal, HistoricalSignal
+from vessim.storage import Storage, SimpleBattery
 
 __all__ = [
-    "signal",
-    "cosim",
-    "actor",
-    "controller",
-    "storage",
-    "power_meter",
+    "Actor",
+    "ComputingSystem",
+    "Generator",
+    "Controller",
+    "Monitor",
+    "Microgrid",
+    "Environment",
+    "MicrogridPolicy",
+    "DefaultMicrogridPolicy",
+    "PowerMeter",
+    "MockPowerMeter",
+    "Signal",
+    "HistoricalSignal",
+    "Storage",
+    "SimpleBattery",
 ]
 
 try:
-    import vessim.sil  # noqa: F401
-    __all__.append("sil")
+    from vessim.sil import Broker, SilController, WatttimeSignal, get_latest_event  # noqa: F401
+    __all__.extend(["Broker", "SilController", "WatttimeSignal", "get_latest_event"])
 except ImportError:
     pass
