e0219e6fd7a14dc4fbd95946ab0ab0b2377a86a0
marvin.steinke <marvin.steinke@tu-berlin.de>
2024-06-21 12:08:20 +0200

    added ActorBase to init

--- vessim/__init__.py
+++ vessim/__init__.py
@@ -1,5 +1,6 @@
 """A simulator for carbon-aware applications and systems."""
-from vessim.actor import Actor, ComputingSystem
+
+from vessim.actor import ActorBase, Actor, ComputingSystem
 from vessim.controller import Controller, Monitor
 from vessim.cosim import Microgrid, Environment
 from vessim.policy import MicrogridPolicy, DefaultMicrogridPolicy
@@ -7,6 +8,7 @@ from vessim.signal import Signal, HistoricalSignal, MockSignal
 from vessim.storage import Storage, SimpleBattery
 
 __all__ = [
+    "ActorBase",
     "Actor",
     "ComputingSystem",
     "Controller",
@@ -24,6 +26,7 @@ __all__ = [
 
 try:
     from vessim.sil import Broker, SilController, WatttimeSignal, get_latest_event  # noqa: F401
+
     __all__.extend(["Broker", "SilController", "WatttimeSignal", "get_latest_event"])
 except ImportError:
     pass
